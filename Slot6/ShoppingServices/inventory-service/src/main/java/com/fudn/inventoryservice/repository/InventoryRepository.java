a74e69c4fac678d1ccc97b0b5e07cbe7c7fc1e60
Tran Duc <duct23333@gmail.com>
2026-09-25 15:55:57 +0700

    chore: init Slot6 ShoppingServices and Part 2 documentation

---
+++ Slot6/ShoppingServices/inventory-service/src/main/java/com/fudn/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.fudn.inventoryservice.repository;
+
+import com.fudn.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
